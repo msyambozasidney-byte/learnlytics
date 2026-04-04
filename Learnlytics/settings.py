3830e79e06b398801ad6445f18d87da6de69d75e
Sidney <msyambozasidney@gimail.com>
2026-04-04 10:43:09 +0200

    Fix loading issue

--- Learnlytics/settings.py
+++ Learnlytics/settings.py
@@ -126,9 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CmpressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = []
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = 'home'
